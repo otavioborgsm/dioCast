09b22e7ae6d29cbe80099d2f6ae7a7b5552e0568
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 09:49:12 -0300

    iniciando home

---
+++ dio_cast/android/app/src/main/kotlin/com/example/dio_cast/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dio_cast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dio_cast/lib/home/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../util/custom_color.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:<Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        SvgPicture.asset("assets/logo.svg",
+                            width: 40, height: 40)
+                      ],
+                    ),
+                    Row(
+                      children: const [
+                        Text(
+                          "Dio",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "Cast",
+                          style: TextStyle(
+                            color: CustomColor.pink700,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
